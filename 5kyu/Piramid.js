d215943b1139b02bbb6aa9313994b94fdf96565b
SSSardor <sardorsamijonov1226@gmail.com>
2022-04-27 12:13:08 +0500

    pyramid * orqali

---
+++ 5kyu/Piramid.js
@@ -0,0 +1,14 @@
+function towerBuilder(nFloors) {
+  var tower = [];
+  for (var i = 0; i < nFloors; i++) {
+    tower.push(
+      ` ${
+        ' '.repeat(nFloors - i - 1) +
+        '*'.repeat(i * 2 + 1) +
+        ' '.repeat(nFloors - i - 1)
+      }`
+    );
+  }
+  return tower;
+}
+console.log(towerBuilder(10));
